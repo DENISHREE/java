e13ef6f25a936a40eedcb7b485bec11ca699034a
DENISHREE L <717822p310@kce.ac.in>
2024-12-17 20:52:41 +0530

    Create  Divide Two Integers.java

---
+++ Divide Two Integers.java
@@ -0,0 +1,32 @@
+
+public class Solution {
+    public int divide(int dividend, int divisor) {
+        if (dividend == Integer.MIN_VALUE && divisor == -1) {
+            return Integer.MAX_VALUE;
+        }
+        boolean negative = (dividend < 0) ^ (divisor < 0);
+        long absDividend = Math.abs((long) dividend);
+        long absDivisor = Math.abs((long) divisor);
+        long quotient = 0;
+        while (absDividend >= absDivisor) {
+            long tempDivisor = absDivisor, multiple = 1;
+            while (absDividend >= (tempDivisor << 1)) {
+                tempDivisor <<= 1;
+                multiple <<= 1;
+            }
+            absDividend -= tempDivisor;
+            quotient += multiple;
+        }
+        if (negative) {
+            quotient = -quotient;
+        }
+        return (int) quotient;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.divide(10, 3));
+        System.out.println(solution.divide(7, -3)); 
+        System.out.println(solution.divide(-2147483648, -1)); 
+    }
+}
