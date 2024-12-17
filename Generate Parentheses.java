041c3dacb1e07c90c1a09bb84aa8d84e7dcf7e60
DENISHREE L <717822p310@kce.ac.in>
2024-12-17 20:35:35 +0530

    Create Generate Parentheses.java

---
+++ Generate Parentheses.java
@@ -0,0 +1,31 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+
+    public static List<String> generateParenthesis(int n) {
+        List<String> result = new ArrayList<>();
+        backtrack(result, "", 0, 0, n);
+        return result;
+    }
+    private static void backtrack(List<String> result, String current, int open, int close, int max) {
+        if (current.length() == max * 2) {
+            result.add(current);
+            return;
+        }
+        if (open < max) {
+            backtrack(result, current + "(", open + 1, close, max);
+        }
+        if (close < open) {
+            backtrack(result, current + ")", open, close + 1, max);
+        }
+    }
+
+    public static void main(String[] args) {
+        int n1 = 3;
+        System.out.println(generateParenthesis(n1)); 
+        int n2 = 1;
+        System.out.println(generateParenthesis(n2)); 
+    }
+}
