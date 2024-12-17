6b6cfa6129104a5a9cfded622342e63500224137
DENISHREE L <717822p310@kce.ac.in>
2024-12-17 20:56:48 +0530

    Create  Search in Rotated Sorted Array.java

---
+++ Search in Rotated Sorted Array.java
@@ -0,0 +1,40 @@
+
+class Solution {
+    public int search(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            if (nums[left] <= nums[mid]) {
+                if (nums[left] <= target && target < nums[mid]) {
+                    right = mid - 1; 
+                } else {
+                    left = mid + 1; 
+                }
+            } else {
+                if (nums[mid] < target && target <= nums[right]) {
+                    left = mid + 1; 
+                } else {
+                    right = mid - 1; 
+                }
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int[] nums1 = {4, 5, 6, 7, 0, 1, 2};
+        int target1 = 0;
+        System.out.println("Output: " + sol.search(nums1, target1)); 
+        int[] nums2 = {4, 5, 6, 7, 0, 1, 2};
+        int target2 = 3;
+        System.out.println("Output: " + sol.search(nums2, target2)); 
+        int[] nums3 = {1};
+        int target3 = 0;
+        System.out.println("Output: " + sol.search(nums3, target3)); 
+    }
+}
