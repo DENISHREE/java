3fcfa270b31e033d813f39078ea0e1ddf72153aa
DENISHREE L <717822p310@kce.ac.in>
2024-12-17 21:07:54 +0530

    Create Rotate Image.java

---
+++ Rotate Image.java
@@ -0,0 +1,47 @@
+
+public class Solution {
+
+    public static void rotate(int[][] matrix) {
+        int n = matrix.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n / 2; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][n - j - 1];
+                matrix[i][n - j - 1] = temp;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix1 = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+        rotate(matrix1);
+        printMatrix(matrix1);  
+        int[][] matrix2 = {
+            {5, 1, 9, 11},
+            {2, 4, 8, 10},
+            {13, 3, 6, 7},
+            {15, 14, 12, 16}
+        };
+        rotate(matrix2);
+        printMatrix(matrix2);  
+    }
+    private static void printMatrix(int[][] matrix) {
+        for (int[] row : matrix) {
+            for (int val : row) {
+                System.out.print(val + " ");
+            }
+            System.out.println();
+        }
+    }
+}
