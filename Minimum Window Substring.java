1f6fddb1e12e08a310d361aa20d1c755e2caf118
DENISHREE L <denishree2004@gmail.com>
2024-12-24 10:50:34 +0530

    Create Minimum Window Substring.java

---
+++ Minimum Window Substring.java
@@ -0,0 +1,42 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public String minWindow(String s, String t) {
+        if (s == null || t == null || s.length() < t.length()) {
+            return "";
+        }
+        Map<Character, Integer> tFreq = new HashMap<>();
+        for (char c : t.toCharArray()) {
+            tFreq.put(c, tFreq.getOrDefault(c, 0) + 1);
+        }
+        int left = 0, right = 0;
+        int required = tFreq.size(); 
+        int formed = 0;              
+        Map<Character, Integer> windowFreq = new HashMap<>();
+        int[] result = {-1, 0, 0}; 
+        while (right < s.length()) {
+            char c = s.charAt(right);
+            windowFreq.put(c, windowFreq.getOrDefault(c, 0) + 1);
+            if (tFreq.containsKey(c) && windowFreq.get(c).intValue() == tFreq.get(c).intValue()) {
+                formed++;
+            }
+            while (left <= right && formed == required) {
+                c = s.charAt(left);
+                if (result[0] == -1 || right - left + 1 < result[0]) {
+                    result[0] = right - left + 1;
+                    result[1] = left;
+                    result[2] = right;
+                }
+                windowFreq.put(c, windowFreq.get(c) - 1);
+                if (tFreq.containsKey(c) && windowFreq.get(c).intValue() < tFreq.get(c).intValue()) {
+                    formed--;
+                }
+                left++;
+            }
+            right++;
+        }
+
+        return result[0] == -1 ? "" : s.substring(result[1], result[2] + 1);
+    }
+}
